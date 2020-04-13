d01f22a18ee0abbd5cf877823be396335005f356
TEJENDRARANA <TEJENDRARANA@9.79.244.136>
2020-04-13 14:04:05 +0530

    9

--- src/main/java/com/tsr/s1moviecatalogservice/resorce/MovieCatalogResourceDefault.java
+++ src/main/java/com/tsr/s1moviecatalogservice/resorce/MovieCatalogResourceDefault.java
@@ -5,11 +5,13 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+//import org.apache.tomcat.util.http.fileupload.RequestContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.support.RequestContext;
 
 import com.tsr.s1moviecatalogservice.model.CatalogItem;
 import com.tsr.s1moviecatalogservice.model.Movie;
@@ -20,15 +22,17 @@ import com.tsr.s1moviecatalogservice.model.UserRating;
 //@RequestMapping("/")
 public class MovieCatalogResourceDefault {
 	
+	static int cnt=100;
+	
 	@Autowired
 	private RestTemplate restTemplate;
 	
 	@RequestMapping("/")
 	public List<CatalogItem> getCatalog(){
 		
-		System.out.println("TSR -- Default Calling...");
-		MovieCatalogResource a = new MovieCatalogResource();
-		return a.getCatalog("TSR..9");
+		System.out.println("TSR 1 -- Default Calling...");
+		cnt++;
+		return restTemplate.getForObject("http://s1-movie-catalog-service:8081/catalog/TSR.."+cnt, List.class);
 	}
 	
 	@RequestMapping("/movie/{movieId}")
